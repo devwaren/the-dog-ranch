b3735766507593120c5f9c9a28e7746ca3921d24
Waren Gador <waren.gador15@gmail.com>
2025-09-10 02:34:21 +0800

    add all files

--- server.ts
+++ server.ts
@@ -62,9 +62,9 @@ app.use('*all', async (req, res) => {
 
     res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
   } catch (e) {
-    vite?.ssrFixStacktrace(e)
-    console.log(e.stack)
-    res.status(500).end(e.stack)
+    vite?.ssrFixStacktrace(e as Error)
+    console.log((e as Error).stack)
+    res.status(500).end((e as Error).stack)
   }
 })
 
